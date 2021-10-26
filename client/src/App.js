c98ecd128b39d940f2890e00187c432a3c015f19
Maicon de Morais <68478805+maiconmrs@users.noreply.github.com>
2021-10-26 14:43:11 +0200

    Fix: alignment

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import { Route } from "react-router-dom";
-import Homepage from "./components/Homepage";
-import Game from "./components/Game";
+
 import GamePage from "./pages/GamePage";
 
 const App = () => {
   return (
     <div style={{ color: "black" }}>
-      <Route path="/play" exact component={Game} />
-      <Route path="/game" exact component={GamePage} />
-
-      <Route path="/" exact component={Homepage} />
+      <Route path="/" exact component={GamePage} />
     </div>
   );
 };
